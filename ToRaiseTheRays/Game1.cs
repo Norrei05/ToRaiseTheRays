7f99e7bbf52ede803d4f53625aafde1054541219
Norrei05 <ads2050@rit.edu>
2025-03-21 16:08:10 -0400

    Added/loaded in most external visual assets and created map (no scrolling yet).

--- ToRaiseTheRays/Game1.cs
+++ ToRaiseTheRays/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 
 namespace ToRaiseTheRays;
@@ -15,7 +16,14 @@ public class Game1 : Game
 
     private Dictionary<string, Texture2D> playerSprites;
     public static Texture2D BulletTexture { get; private set; }
+
     // private Texture2D fogTexture;
+
+    private Texture2D dayTileset;
+
+    private Texture2D nightTileset;
+
+    private Rectangle[,] map;
 
     private Player player;
     public static List<GameObject> ActiveEntities { get; private set; }
@@ -39,6 +47,10 @@ public class Game1 : Game
     protected override void Initialize()
     {
         ScreenBounds = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+
+        // Initialize empty 2D Rectangle array for the future map
+        map = new Rectangle[ScreenBounds.Width / 16 + 1, (ScreenBounds.Height / 16) * 2];
+
         base.Initialize();
     }
 
@@ -47,7 +59,7 @@ public class Game1 : Game
         SpriteBatch = new SpriteBatch(GraphicsDevice);
 
         PapyrusFont = Content.Load<SpriteFont>("Papyrus");
-        BulletTexture = Content.Load<Texture2D>("Bullet");
+        BulletTexture = Content.Load<Texture2D>("Bullet2");
 
         // Load all player sprites
         foreach (string direction in new string[]{ "N", "U", "D", "L", "R", "UL", "UR", "DL", "DR" })
@@ -55,9 +67,17 @@ public class Game1 : Game
             playerSprites["Barque_" + direction] = Content.Load<Texture2D>($"PlayerSprites/Barque_{direction}");
         }
 
+        // Load in fog of war asset
+        // fogTexture = Content.Load<Texture2D>("Fog_Of_War");
+
+        // Load in tilesets and generate a map of source rectangles based on placement of tiles in tileset images
+        dayTileset = Content.Load<Texture2D>("Tiles_Day_1");
+        nightTileset = Content.Load<Texture2D>("Tiles_Night_1");
+        CreateMap();
+
         // Create player of size 44x44 at the bottom of the screen
         Rectangle playerPos = new(ScreenBounds.Width / 2 - 22, ScreenBounds.Height - 100, 44, 44);
-        player = new Player(playerSprites, playerPos, null);  // Passing null instead of fogTexture
+        player = new Player(playerSprites, playerPos, null); // Passing null instead of fogTexture
         ActiveEntities.Add(player);
     }
 
@@ -91,13 +111,92 @@ public class Game1 : Game
         base.Update(gameTime);
     }
 
-    protected override void Draw(GameTime gameTime) {
+    protected override void Draw(GameTime gameTime) {        
         GraphicsDevice.Clear(Color.Sienna);
 
         SpriteBatch.Begin();
+
+        // Loop through map, displaying it to screen
+        for (int col = 0; col < map.GetLength(0); col++)
+        {
+            for (int row = 0; row < map.GetLength(1); row++)
+                SpriteBatch.Draw(nightTileset, new Vector2(col * 16, row * 16), map[col, row], Color.White);
+        }
+
         foreach (GameObject entity in ActiveEntities) entity.Draw();
+
         SpriteBatch.End();
     
         base.Draw(gameTime);
+    }
+
+    /// <summary>
+    /// Helper method to create map out of tilesets based on the pre-determined dimensions of the tilesets.
+    /// </summary>
+    private void CreateMap()
+    {
+        if (map == null)
+            return;
+
+        Rectangle[,] tileDivides = new Rectangle[7, 8];
+
+        // Loop through empty 2D array and fill it with the dimensions of each tile
+        for (int col = 0; col < tileDivides.GetLength(0); col++)
+        {
+            for (int row = 0; row < tileDivides.GetLength(1); row++)
+                tileDivides[col, row] = new Rectangle(col * 16, row * 16, 16, 16);
+        }
+
+        // Grab the tiles needed from 2D array and then loop through each row, adding the same river pattern to the right side
+        Rectangle sandToGrass = tileDivides[2, 2];
+        Rectangle grass = tileDivides[2, 0];
+        Rectangle grassToSand = tileDivides[0, 2];
+        Rectangle sandToWater = tileDivides[5, 2];
+        Rectangle water = tileDivides[6, 2];
+
+        for (int row = 0; row < map.GetLength(1); row++)
+        {
+            map[map.GetLength(0) - 1, row] = water;
+            map[map.GetLength(0) - 2, row] = water;
+            map[map.GetLength(0) - 3, row] = water;
+            map[map.GetLength(0) - 4, row] = water;
+            map[map.GetLength(0) - 5, row] = sandToWater;
+            map[map.GetLength(0) - 6, row] = grassToSand;
+            map[map.GetLength(0) - 7, row] = grass;
+            map[map.GetLength(0) - 8, row] = sandToGrass;
+        }
+
+        // Populate the rest of the map with randomized sand tiles, favoring empty sand over sand with rocks
+        Random randomGen = new Random();
+
+        int tile;
+
+        for (int col = 0; col < map.GetLength(0) - 8; col++)
+        {
+            for (int row = 0; row < map.GetLength(1); row++)
+            {
+                tile = randomGen.Next(0, 10);
+
+                switch (tile)
+                {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                        map[col, row] = tileDivides[0, 0];
+                        break;
+                    case 8:
+                        map[col, row] = tileDivides[5, 0];
+                        break;
+                    case 9:
+                        map[col, row] = tileDivides[6, 0];
+                        break;
+                }
+            }
+        }
     }
 }
